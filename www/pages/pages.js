e0571207941c31ac5b1c4911c406c31ec22f334a
Maneesh Tewani <mtewani@purdue.edu>
2016-10-03 17:33:49 -0400

    Finished functionality

--- www/pages/pages.js
+++ www/pages/pages.js
@@ -1,5 +1,4 @@
 function toggle(id) {
     var element = document.getElementById(id);
-    console.log(element);
     element.style.visibility = element.style.visibility === "hidden" ? "visible" : "hidden";
 }
